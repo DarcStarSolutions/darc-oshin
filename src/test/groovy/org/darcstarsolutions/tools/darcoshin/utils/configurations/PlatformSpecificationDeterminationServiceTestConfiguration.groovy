b327f204e5bb80671aa734e17349617f5fa24c95
mharris021 <mharris021@hotmail.com>
2018-02-27 16:44:55 -0600

    add property to get appveyor to work

--- src/test/groovy/org/darcstarsolutions/tools/darcoshin/utils/configurations/PlatformSpecificationDeterminationServiceTestConfiguration.groovy
+++ src/test/groovy/org/darcstarsolutions/tools/darcoshin/utils/configurations/PlatformSpecificationDeterminationServiceTestConfiguration.groovy
@@ -3,9 +3,10 @@ package org.darcstarsolutions.tools.darcoshin.utils.configurations
 import org.darcstarsolutions.tools.darcoshin.PlatformSpecification.Platform
 import org.darcstarsolutions.tools.darcoshin.utils.PlatformSpecificationDeterminationService
 import org.darcstarsolutions.tools.darcoshin.utils.PlatformSpecificationDeterminationServiceImpl
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 
 @TestConfiguration
 class PlatformSpecificationDeterminationServiceTestConfiguration {
@@ -16,13 +17,19 @@ class PlatformSpecificationDeterminationServiceTestConfiguration {
     }
 
     @Bean
-    @ConditionalOnProperty(name = "darc-oshin.platform", havingValue = "linux", matchIfMissing = true)
+    @Profile("linux")
     Platform linuxPlatform() {
         return Platform.LINUX
     }
 
     @Bean
-    @ConditionalOnProperty(name = "darc-oshin.platform", havingValue = "windows")
+    @ConditionalOnMissingBean
+    Platform defaultPlatform() {
+        return Platform.LINUX
+    }
+
+    @Bean
+    @Profile("windows")
     Platform windowsPlatform() {
         return Platform.WINDOWS
     }
