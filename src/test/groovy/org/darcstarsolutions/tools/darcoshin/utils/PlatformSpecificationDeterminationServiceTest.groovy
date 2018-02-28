4a83a07c5a48479fc475aca306f3ed0484f829ea
mharris021 <mharris021@hotmail.com>
2018-02-27 20:38:15 -0600

    add property to get appveyor to work

--- src/test/groovy/org/darcstarsolutions/tools/darcoshin/utils/PlatformSpecificationDeterminationServiceTest.groovy
+++ src/test/groovy/org/darcstarsolutions/tools/darcoshin/utils/PlatformSpecificationDeterminationServiceTest.groovy
@@ -4,6 +4,8 @@ import org.darcstarsolutions.tools.darcoshin.PlatformSpecification
 import org.darcstarsolutions.tools.darcoshin.utils.configurations.PlatformSpecificationDeterminationServiceTestConfiguration
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -13,6 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = PlatformSpecificationDeterminationServiceTestConfiguration.class)
 class PlatformSpecificationDeterminationServiceTest {
+
+    static Logger LOGGER = LoggerFactory.getLogger(PlatformSpecificationDeterminationServiceTest.class)
 
     @Autowired
     PlatformSpecificationDeterminationService platformSpecificationDeterminationService
@@ -24,6 +28,7 @@ class PlatformSpecificationDeterminationServiceTest {
     void testExistence() {
         assertThat(platformSpecificationDeterminationService) isNotNull()
         assertThat(platform).isNotNull()
+        LOGGER.info("Active Profile: " + System.getProperty("spring.profiles.active"))
     }
 
     @Test
