7851365271d6b4999f9016b2a173b06eaa34ab71
mharris021 <mharris021@hotmail.com>
2018-02-28 15:41:39 -0600

    added platform architecture test

--- src/test/groovy/org/darcstarsolutions/tools/darcoshin/utils/configurations/PlatformSpecificationDeterminationServiceTestConfiguration.groovy
+++ src/test/groovy/org/darcstarsolutions/tools/darcoshin/utils/configurations/PlatformSpecificationDeterminationServiceTestConfiguration.groovy
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 
+import static org.darcstarsolutions.tools.darcoshin.PlatformSpecification.PlatformArchitecture.BIT_32
 import static org.darcstarsolutions.tools.darcoshin.PlatformSpecification.PlatformArchitecture.BIT_64
 import static org.darcstarsolutions.tools.darcoshin.PlatformSpecification.PlatformType
 import static org.darcstarsolutions.tools.darcoshin.PlatformSpecification.PlatformType.LINUX
@@ -42,6 +43,18 @@ class PlatformSpecificationDeterminationServiceTestConfiguration {
     @Bean
     @ConditionalOnMissingBean
     PlatformArchitecture defaultPlatformArchitecture() {
+        return BIT_64
+    }
+
+    @Bean
+    @Profile("32-bit")
+    PlatformArchitecture bit32PlatformArchitecture() {
+        return BIT_32
+    }
+
+    @Bean
+    @Profile("64-bit")
+    PlatformArchitecture bit64PlatformArchitecture() {
         return BIT_64
     }
 
